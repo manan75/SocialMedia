d0646170dc0581c7bcba8f2d58f76ebc59040837
manan75 <4696.stkabirdin@gmail.com>
2025-02-14 01:06:38 +0530

    Started login page

--- react-app/src/Pages/Login/Login.jsx
+++ react-app/src/Pages/Login/Login.jsx
@@ -1,8 +1,29 @@
 
 import React from 'react'
-
-export const Login = () => {
+import './Login.scss';
+ function Login(){
   return (
-    <div>Login</div>
+    <div className='Login'>
+        <div className='card'>
+            <div className='left'>
+                <h1>Hello world</h1>
+                <p>
+                    loream ipsum i loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i
+                </p>
+                <span>Don't have an account?</span>
+                <button> Register</button>
+            </div>
+            <div className='right'>
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder='Username'>
+                    </input>
+                    <input type="text" placeholder='Password'></input>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
+export default Login;
