be22903508275a1bf0d6ea35a6622b99cc7f8946
manan75 <4696.stkabirdin@gmail.com>
2025-02-15 00:03:31 +0530

    designed login and register

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 function App() {
   return (
     <div >
-     <Login/>
+     <Register/>
     </div>
   );
 }
---
+++ react-app/src/Pages/Register/Register.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import './Register.scss'
+ function Register(){
+  return (
+    <div className='Register'>
+        <div className='card'>
+            <div className='left'>
+                <h1>Hello world</h1>
+                <p>
+                    loream ipsum i loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i  loream ipsum i
+                </p>
+                <span>Do you have an account?</span>
+                <button> Login</button>
+            </div>
+            <div className='right'>
+                <h1>Register</h1>
+                <form>
+                    <input type="text" placeholder='Username'>
+                    </input>
+                    <input type="email" placeholder='Email'></input>
+                    <input type="password" placeholder='Password'></input>
+                    <input type="text" placeholder='Name'></input>
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Register;
