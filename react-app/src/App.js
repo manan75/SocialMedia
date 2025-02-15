b2e4ffa1b55c591194a1203b49b9391ad17e5369
manan75 <4696.stkabirdin@gmail.com>
2025-02-15 15:05:30 +0530

    started frontend main parts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,19 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
+import Navbar from './Pages/Components/Navbar';
+import LeftBar from './Pages/Components/LeftBar';
+import RightBar from './Pages/Components/RightBar';
+import Home from "./Pages/Home/home"
+import Profile from './Pages/Profile/Profile';
 import {
   createBrowserRouter,
   RouterProvider,
-  Route
+  Route,
+  Outlet,
+  Navigate
 } from 'react-router-dom';
 function App() {
- 
-  const Layout = () =>{  //for variable users
+ const currentUser = true;  //for test , if i do false it will get protected routed i.e user isnt logged in
+  const Layout = () =>{  //for variable users , outlet allows other children to be visible there
     return(
-      <div></div>
+      <div>
+        <Navbar/>
+        <div style={{display: "flex"}}>
+          <LeftBar/>
+          <Outlet/>                  
+          <RightBar/>
+        </div>
+      </div>
     )
   }
+  const ProtectedRoute = ({children}) =>{      //if user is logged in it will return to login page
+    if(!currentUser){
+      return <Navigate to="/login"></Navigate>
+    }
+    return children
+  }
+  
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<ProtectedRoute><Layout/></ProtectedRoute> ,
+      children:[
+        {
+          path: "/home",
+          element:<Home/>
+        },
+        {
+          path:"/profile/:id",
+          element:<Profile/>
+        }
+      ]
+    },
     {
       path: "/login",
       element: <Login/>
--- react-app/src/Pages/Components/Navbar.jsx
+++ react-app/src/Pages/Components/Navbar.jsx
@@ -1,10 +1,28 @@
 import './Navbar.scss'
-
+import GridViewIcon from '@mui/icons-material/GridView';
+import HomeIcon from '@mui/icons-material/Home';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='left'>
+        <Link to='/home' style={{textDecoration:"none"}}>
+        <span>MananSocial</span>
+        </Link> 
+        <HomeIcon/>
+        <GridViewIcon/>
+        <DarkModeIcon/>
+        <PersonOutlineOutlinedIcon/>
+        <SearchOutlinedIcon/>
+      </div>
+      <div className='right'>
+
+      </div>
+    </div>
   )
 }
 
